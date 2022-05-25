974f794ec58139db1dda459a86605d78ac200588
matdulgi <yrdog9494@gmail.com>
2022-05-25 20:58:23 +0900

    fix native error, move to /opt, and so on

--- hadoop-env.sh
+++ hadoop-env.sh
@@ -1,6 +1,6 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64
 
-export HADOOP_HOME=/usr/local/hadoop
+export HADOOP_HOME=/opt/hadoop
 export HADOOP_HDFS_HOME=$HADOOP_HOME
 export HADOOP_MAPRED_HOME=$HADOOP_HOME
 export HADOOP_COMMON_HOME=$HADOOP_HOME
